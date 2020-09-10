7c545824d93735312b04997e51e2ac0c3b36da0b
NoahDMoore <noahsemailis@gmail.com>
2020-09-09 21:21:14 -0500

    Error fix.

--- bin/pikite.sh
+++ bin/pikite.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-if [ $1 = "start" ] then
+if [ "$1" = "start" ] ; then
 
 sudo python3 /home/pi/pikite/PiKite.py
 
 fi
 
-if [ $1 = "stop" ] then
+if [ "$1" = "stop" ] ; then
 
 sudo pkill -f PiKite.py
 sudo pkill -f websocket_server.py
 
 fi
 
-if [ $1 = "restart" ] then
+if [ "$1" = "restart" ] ; then
 
 sudo pkill -f PiKite.py
 sudo pkill -f websocket_server.py
