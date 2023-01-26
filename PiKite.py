7052d3b765e0cc1c09e9489aecb1f90bddceee4d
Noah <noahsemailis@gmail.com>
2023-01-25 19:21:07 -0600

    Reset menu after focus.

--- PiKite.py
+++ PiKite.py
@@ -473,6 +473,8 @@ def focus_camera():
 	while program_state == "focusCamera":
 		camera.capture("/home/pi/pikite/output/photos/focus.jpg")
 		time.sleep(2)
+
+	menu.restart()
 	
 
 def print_one_line(message):
