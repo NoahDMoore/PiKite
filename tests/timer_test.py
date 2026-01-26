866e41f40284b0895e28f2bab69de16e0b682a81
Noah Moore <noahsemailis@gmail.com>
2026-01-25 20:45:55 -0600

    Change logging level

--- tests/timer_test.py
+++ tests/timer_test.py
@@ -10,6 +10,7 @@ import time
 
 # Setup Logger
 logger = get_logger(__name__)
+logger.setLevel("DEBUG")
 
 logger.info("Starting Timer Tests")
 
@@ -19,6 +20,8 @@ def test_timer_start_stop():
     logger.info("Timer started")
     time.sleep(5)
     elapsed = timer.stop()
+    if elapsed is None:
+        raise AssertionError("Elapsed time should not be None after stopping the timer")
     assert 4.9 < elapsed < 5.1, f"Elapsed time should be around 5 seconds, got {elapsed}"
     logger.info(f"Timer stopped, elapsed time: {elapsed} seconds")
 
@@ -30,12 +33,16 @@ def test_timer_pause_resume():
     timer.pause()
     logger.info("Timer paused")
     paused_elapsed = timer.elapsed()
+    if paused_elapsed is None:
+        raise AssertionError("Elapsed time should not be None when timer is paused")
     assert 2.9 < paused_elapsed < 3.1, f"Elapsed time should be around 3 seconds, got {paused_elapsed}"
     time.sleep(2)  # This should not count towards elapsed time
     timer.resume()
     logger.info("Timer resumed")
     time.sleep(2)
     total_elapsed = timer.stop()
+    if total_elapsed is None:
+        raise AssertionError("Elapsed time should not be None after stopping the timer")
     assert 4.9 < total_elapsed < 5.1, f"Total elapsed time should be around 5 seconds, got {total_elapsed}"
     logger.info(f"Timer stopped, total elapsed time: {total_elapsed} seconds")
 
