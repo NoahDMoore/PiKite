74a1eca89f654ce026d4fe6388370486bb7ac733
Noah Moore <noahsemailis@gmail.com>
2026-01-31 20:49:40 -0600

    Add PanTiltPattern test

--- tests/servo_controller_test.py
+++ tests/servo_controller_test.py
@@ -4,7 +4,8 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parents[1] / "src"))
 
 from pikite.core.logger import get_logger
-from pikite.hardware.servo_controller import TiltServo, PanServo, DIRECTION
+from pikite.core.timer import Timer
+from pikite.hardware.servo_controller import TiltServo, PanServo, PanTiltPattern, DIRECTION
 
 import time
 
@@ -59,3 +60,22 @@ def test_pan_servo_rotate():
     time.sleep(3)  # Rotate for 3 seconds
     pan_servo.stop()
     logger.info("PanServo stopped successfully")
+
+def test_pan_tilt_pattern(pattern):
+    pan_servo = PanServo()
+    assert pan_servo is not None
+    logger.info("PanServo initialized successfully")
+                
+    tilt_servo = TiltServo()
+    assert tilt_servo is not None
+    logger.info("TiltServo initialized successfully")
+
+    mode = PanTiltPattern.PAN_TILT_MODES[pattern]
+
+    timer = Timer()
+    timer.start()
+    pan_tilt_pattern = PanTiltPattern(mode, pan_servo, tilt_servo)
+
+    while timer.elapsed() < 300.0: # type: ignore (to suppress mypy warning; elapsed() cannot return None if the timer is running or paused)
+        if timer.interval_elapsed(5, "pattern_test"):
+            pan_tilt_pattern.step()
