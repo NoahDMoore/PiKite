99d3aca40f0a789f7145da3ae06e4cab4708b55b
Noah Moore <noahsemailis@gmail.com>
2026-02-15 19:07:23 -0600

    Cap tilt_servo tests at 90 degrees

--- tests/servo_controller_test.py
+++ tests/servo_controller_test.py
@@ -32,12 +32,6 @@ def test_tilt_servo_move_to_position():
     tilt_servo.angle = 90  # Move to 90 degrees
     logger.info("TiltServo moved to 90 degrees successfully")
     time.sleep(2)
-    tilt_servo.angle = 135  # Move to 135 degrees
-    logger.info("TiltServo moved to 135 degrees successfully")
-    time.sleep(2)
-    tilt_servo.angle = 180  # Move to 180 degrees
-    logger.info("TiltServo moved to 180 degrees successfully")
-    time.sleep(2)
 
 # Pan Servo Tests
 def test_pan_servo_initialization():
