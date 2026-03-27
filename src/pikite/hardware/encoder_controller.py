1c480a8597122a3dbcf69cb01f24eeba25cbd0a2
Noah Moore <noahsemailis@gmail.com>
2026-03-26 22:29:19 -0500

    Update EncoderController

--- src/pikite/hardware/encoder_controller.py
+++ src/pikite/hardware/encoder_controller.py
@@ -1,27 +1,28 @@
 import smbus    # type: ignore
 
-DEVICE_AS5600 = 0x36 # Default device I2C address
-bus = smbus.SMBus(1)
-
 class EncoderController:
     """Controller for the AS5600 Magnetic Rotary Encoder to measure angle."""
+    DEVICE_AS5600 = 0x36 # Default device I2C address
 
-    def __init__(self):
+    def __init__(self, bus_number=1):
         """Initialize the EncoderController and AS5600 sensor."""
+        self.bus = smbus.SMBus(bus_number)
+
         self.zero()  # Set the initial zero point
+
+    def _read_raw_angle(self): # Read angle (0-360 represented as 0-4096)
+        read_bytes = self.bus.read_i2c_block_data(self.DEVICE_AS5600, 0x0C, 2)
+        value = (read_bytes[0] << 8) | read_bytes[1]
+        return value & 0x0FFF # Mask to 12 bits
 
     def get_angle(self):
         """Get the current angle."""
-        self._raw_angle = read_raw_angle()
-        self.angle = (self._raw_angle - self._zero_point) % 4096
-        return self.angle * (360.0 / 4096.0)
+        raw_angle = self._read_raw_angle()
+        self.angle = (raw_angle - self._zero_point) % 4096
+        return round(self.angle * (360.0 / 4096.0), 2)
     
     def zero(self):
         """Zero the encoder by setting the current angle as the new zero point."""
-        self._raw_angle = read_raw_angle()
-        self._zero_point = self._raw_angle
+        raw_angle = self._read_raw_angle()
+        self._zero_point = raw_angle
         self.angle = 0.0
-
-def read_raw_angle(): # Read angle (0-360 represented as 0-4096)
-    read_bytes = bus.read_i2c_block_data(DEVICE_AS5600, 0x0C, 2)
-    return (read_bytes[0]<<8) | read_bytes[1];
