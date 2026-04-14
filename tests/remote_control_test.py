9d936c4f83d4ff7cdbfead79071d9cd4165aa287
Noah Moore <noahsemailis@gmail.com>
2026-04-13 22:08:45 -0500

    Add tests for updating settings and restoring defaults

--- tests/remote_control_test.py
+++ tests/remote_control_test.py
@@ -20,7 +20,7 @@ input_handler = InputHandler()
 menu = Menu(None, settings, input_handler)
 remote_input = RemoteInput(server, input_handler)
 
-def fetch_settings():
+def fetch_settings(**kwargs):
     current_settings = settings.format_as_dict()
     menu_settings = menu.format_settings_and_options_as_dict()
     settings_payload = {
@@ -31,7 +31,22 @@ def fetch_settings():
 
     server.send(settings_payload)
 
+def update_settings(args):
+    for new_setting, new_setting_value in args.get("settings_to_update", {}).items():
+        if settings.is_setting(new_setting):
+            print(f"Updating setting '{new_setting}' from {settings.get(new_setting)} to new value '{new_setting_value}'")
+            settings.set(new_setting, new_setting_value)
+            fetch_settings()  # Send updated settings back to client
+        else:
+            print(f"Attempted to update unknown setting: {new_setting}")
+
+def default_settings(**kwargs):
+    settings.load_defaults()
+    fetch_settings()  # Send updated settings back to client
+
 input_handler.register("default", InputCommand.FETCH_SETTINGS, fetch_settings)
+input_handler.register("default", InputCommand.UPDATE_SETTINGS, update_settings)
+input_handler.register("default", InputCommand.LOAD_DEFAULT_SETTINGS, default_settings)
 
 async def main():
     await asyncio.gather(
