fdc87d3b8b3ad32e67d7cd50646f6342ce1e3431
Noah Moore <noahsemailis@gmail.com>
2026-04-26 17:53:34 -0500

    Add media test methods

--- tests/remote_control_test.py
+++ tests/remote_control_test.py
@@ -5,12 +5,14 @@ sys.path.append(str(Path(__file__).resolve().parents[1] / "src"))
 
 import asyncio
 
+from pikite.core.constants import CAPTURE_MODES
+from pikite.core.input_handler import InputCommand, InputSource, InputHandler
+from pikite.core.lcd_menu import Menu
 from pikite.core.logger import set_log_level
 from pikite.remote.microdot_server import ControllerServer
-from pikite.core.settings import Settings
-from pikite.core.lcd_menu import Menu
-from pikite.core.input_handler import InputCommand, InputSource, InputHandler
 from pikite.remote.remote_input import RemoteInput
+from pikite.core.settings import Settings
+from pikite.system.storage import StorageManager
 
 set_log_level("DEBUG")
 
@@ -19,6 +21,7 @@ settings = Settings()
 input_handler = InputHandler()
 menu = Menu(None, settings, input_handler)
 remote_input = RemoteInput(server, input_handler)
+storage_manager = StorageManager()
 
 def fetch_settings(**kwargs):
     current_settings = settings.format_as_dict()
@@ -44,9 +47,29 @@ def default_settings(**kwargs):
     settings.load_defaults()
     fetch_settings()  # Send updated settings back to client
 
+def fetch_media_dirs(**kwargs):
+    media_dirs = storage_manager.get_capture_session_dirs()
+    media_dirs_payload = {
+        "type": "media_dirs_update",
+        "media_dirs": media_dirs
+    }
+    server.send(media_dirs_payload)
+
+def fetch_media(args):
+    mode = CAPTURE_MODES.STILL if args.get("mode") == "STILL" else CAPTURE_MODES.VIDEO
+    path = args.get("path")
+    file_paths = storage_manager.get_capture_session_file_names(mode, path)
+    file_paths_payload = {
+        "type": "media_file_paths",
+        "file_paths": file_paths
+    }
+    server.send(file_paths_payload)
+
 input_handler.register("default", InputCommand.FETCH_SETTINGS, fetch_settings)
 input_handler.register("default", InputCommand.UPDATE_SETTINGS, update_settings)
 input_handler.register("default", InputCommand.LOAD_DEFAULT_SETTINGS, default_settings)
+input_handler.register("default", InputCommand.FETCH_MEDIA_DIRS, fetch_media_dirs)
+input_handler.register("default", InputCommand.FETCH_MEDIA, fetch_media)
 
 async def main():
     await asyncio.gather(
