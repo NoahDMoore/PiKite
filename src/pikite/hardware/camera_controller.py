c98be5b531d07d580f328c244eebee5e0005bb13
Noah Moore <noahsemailis@gmail.com>
2026-05-23 00:00:55 -0500

    Increase sleep

--- src/pikite/hardware/camera_controller.py
+++ src/pikite/hardware/camera_controller.py
@@ -411,7 +411,7 @@ class PreviewStream:
     async def stream(self):
         while True:
             if not self.streaming:
-                await asyncio.sleep(0)
+                await asyncio.sleep(1)
                 continue
             
             if self.latest_frame is not None:
